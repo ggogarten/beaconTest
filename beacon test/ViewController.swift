1ff04493b1577590e547ce96598f03df34cee0f4
George G <ggogarten@gmail.com>
2016-06-15 15:37:57 -0400

    Initial Commit, still having issues with CLBeaconRegion UUID setting

--- beacon test/ViewController.swift
+++ beacon test/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    let locationManager = CLLocationManager()
+    
+    let region = CLBeaconRegion(proximityUUID: NSUUID(uuidString:"B9407F30-F5F8-466E-AFF9-25556B57FE6D")! as UUID, identifier: "Estimotes")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse) {
+        
+            locationManager.requestWhenInUseAuthorization()
+        
+        }
+        
+        locationManager.startRangingBeacons(in: region)
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [AnyObject], in region: CLBeaconRegion) {
+        print(beacons)
+    }
 
 }
 
